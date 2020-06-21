51cc348dc7e5dd46151ecd85e6faf858f3fa5e03
rhmdnrhuda <rhmdnrhuda@gmail.com>
2020-06-21 08:11:37 +0700

    initiate state with navigation menu and floating button post

---
+++ app/src/main/java/com/example/lotus/MainActivity.kt
@@ -0,0 +1,113 @@
+package com.example.lotus
+
+import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetBehavior.BottomSheetCallback
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        try {
+            this.supportActionBar!!.hide()
+        } catch (e: NullPointerException) {
+        }
+        setContentView(R.layout.activity_main)
+
+        fabPostAction()
+        navigationMenuLogic()
+    }
+
+    private fun fabPostAction() {
+        val llBottomSheet =
+            findViewById<View>(R.id.bottom_sheet) as LinearLayout
+
+        val bottomSheetBehavior: BottomSheetBehavior<*> = BottomSheetBehavior.from(llBottomSheet)
+        bottomSheetBehavior.isHideable = false
+        bottomSheetBehavior.setBottomSheetCallback(object : BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                if (BottomSheetBehavior.STATE_DRAGGING == newState) {
+                    fab_post.animate().scaleX(0F).scaleY(0F).setDuration(100).start();
+                } else if (BottomSheetBehavior.STATE_COLLAPSED == newState) {
+                    fab_post.animate().scaleX(1F).scaleY(1F).setDuration(100).start();
+                }
+            }
+
+            override fun onSlide(
+                bottomSheet: View,
+                slideOffset: Float
+            ) {
+            }
+        })
+
+    }
+
+    private fun navigationMenuLogic(){
+        val btmNav =
+            findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        val btmNav2  =
+            findViewById<BottomNavigationView>(R.id.bottom_navigation_2)
+
+        btmNav2.menu.findItem(R.id.navigation_calendar).setCheckable(false)
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        btmNav.setOnNavigationItemSelectedListener(BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            btmNav2.menu.findItem(R.id.navigation_calendar).setCheckable(false)
+            setButtonNavChekable(btmNav, true)
+            when (item.itemId) {
+                R.id.navigation_meditation -> {
+                    btmNav.menu.findItem(item.itemId).setChecked(true)
+                    navController.navigate(item.itemId)
+                    true
+                }
+                R.id.navigation_home -> {
+                    btmNav.menu.findItem(item.itemId).setChecked(true)
+                    navController.navigate(item.itemId)
+                    true
+                }
+                R.id.navigation_reflection -> {
+                    btmNav.menu.findItem(item.itemId).setChecked(true)
+                    navController.navigate(item.itemId)
+                    true
+                }
+                R.id.navigation_tipitaka -> {
+                    btmNav.menu.findItem(item.itemId).setChecked(true)
+                    navController.navigate(item.itemId)
+                    true
+                }
+            }
+            false
+        })
+
+        btmNav2.setOnNavigationItemSelectedListener(BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_calendar -> {
+                    btmNav2.menu.findItem(R.id.navigation_calendar).setCheckable(true)
+                    setButtonNavChekable(btmNav, false)
+                    navController.navigate(item.itemId)
+                    true
+                }
+            }
+            false
+        })
+    }
+
+    private fun setButtonNavChekable(btmNav: BottomNavigationView, active: Boolean) {
+        btmNav.getMenu().findItem(R.id.navigation_meditation).setCheckable(active);
+        btmNav.getMenu().findItem(R.id.navigation_reflection).setCheckable(active);
+        btmNav.getMenu().findItem(R.id.navigation_home).setCheckable(active);
+        btmNav.getMenu().findItem(R.id.navigation_tipitaka).setCheckable(active);
+    }
+
+}
