0a2a04099ed02826f43505e2bce999961dd13a6b
rhmdnrhuda <rhmdnrhuda@gmail.com>
2020-06-28 09:17:31 +0700

    Change fragment notification to tipitaka

--- app/src/main/java/com/example/lotus/ui/tipitaka/TipitakaViewModel.kt
+++ app/src/main/java/com/example/lotus/ui/tipitaka/TipitakaViewModel.kt
@@ -1,10 +1,10 @@
-package com.example.lotus.ui.notifications
+package com.example.lotus.ui.tipitaka
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class TipitakaViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is notifications Fragment"
