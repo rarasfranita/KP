b8aeda7668e8a30352a5e2585bbc5d94e5c755b3
rhmdnrhuda <rhmdnrhuda@gmail.com>
2020-07-16 11:41:38 +0700

    Download media and save to download folder

--- app/src/main/java/com/example/lotus/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/lotus/ui/home/HomeActivity.kt
@@ -1,15 +1,29 @@
 package com.example.lotus.ui.home
 
+import android.app.Dialog
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
 import android.view.View
+import android.view.Window
 import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import androidx.navigation.findNavController
 import com.androidnetworking.AndroidNetworking
+import com.androidnetworking.common.Priority
+import com.androidnetworking.error.ANError
+import com.androidnetworking.interfaces.DownloadListener
+import com.androidnetworking.interfaces.DownloadProgressListener
 import com.example.lotus.R
+import com.example.lotus.models.MediaData
 import com.example.lotus.models.Post
 import com.example.lotus.ui.CreatePostActivity
 import com.example.lotus.ui.detailpost.DetailPost
@@ -24,6 +38,7 @@ import kotlinx.android.synthetic.main.fragment_home.*
 class HomeActivity : AppCompatActivity() {
     private val TAG = "HomeActivity"
     private var manager: FragmentManager? = null
+//    var dialog: Dialog? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,7 +47,7 @@ class HomeActivity : AppCompatActivity() {
         } catch (e: NullPointerException) {
         }
         setContentView(R.layout.activity_main)
-
+//        Dialog(this)
         val fabPost = findViewById<View>(R.id.fab_post)
 
         fabPost.setOnClickListener(View.OnClickListener { fabPostOnClick() })
@@ -143,6 +158,73 @@ class HomeActivity : AppCompatActivity() {
         fab_post?.setVisibility(View.VISIBLE)
         manager?.beginTransaction()
             ?.replace(R.id.fragmentHome, HomeFragment())?.commit()
+    }
+
+    fun downloadMedia(medias: ArrayList<MediaData>){
+        val downloadsPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+        Log.d("DOWNLOAD PATH", downloadsPath.toString())
+        for (media in medias) {
+            val fileName = media.link?.removeRange(0, media.link.length-10)
+
+            AndroidNetworking.download(media.link, downloadsPath.toString(), fileName)
+                .setTag("downloadTest")
+                .setPriority(Priority.MEDIUM)
+                .build()
+                .setDownloadProgressListener(object : DownloadProgressListener {
+                    @RequiresApi(Build.VERSION_CODES.O)
+                    override fun onProgress(
+                        bytesDownloaded: Long,
+                        totalBytes: Long
+                    ) {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                            val channel1 = NotificationChannel(
+                                "channelId",
+                                "Progress Notification",
+                                //IMPORTANCE_HIGH = shows a notification as peek notification.
+                                //IMPORTANCE_LOW = shows the notification in the status bar.
+                                NotificationManager.IMPORTANCE_HIGH
+                            )
+                            channel1.description = "Progress Notification Channel"
+                            val manager = getSystemService(
+                                NotificationManager::class.java
+                            )
+                            manager.createNotificationChannel(channel1)
+                        }
+//                        Log.d("Progress", "downloaded: $bytesDownloaded from total $totalBytes")
+//                        val progress = findViewById<TextView>(R.id.progressDownload)
+//                        progress.text = "$bytesDownloaded/$totalBytes"
+                    }
+                })
+                .startDownload(object : DownloadListener {
+                    override fun onDownloadComplete() {
+                        Log.d("Complete", "TEEE")
+                        Toast.makeText(this@HomeActivity, "Download Complete", Toast.LENGTH_SHORT).show()
+//                        dialog?.dismiss()
+                        // do anything after completion
+                    }
+
+                    override fun onError(error: ANError?) {
+                        // handle error
+                        Log.d("Error download", error?.errorCode.toString())
+                        Log.d("Error download", error!!.errorDetail)
+                    }
+                })
+        }
+    }
+
+    fun showDialog(medias: ArrayList<MediaData>) {
+        val dialog = Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(false)
+        dialog.setContentView(R.layout.layout_menu_post)
+        val download = dialog.findViewById<LinearLayout>(R.id.downloadMedia)
+        download.setOnClickListener {
+            downloadMedia(medias)
+            dialog.dismiss()
+        }
+//        noBtn.setOnClickListener { dialog.dismiss() }
+        dialog.show()
+
     }
 
 }
